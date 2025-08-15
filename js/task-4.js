29edfefa3019f7226ecabdd2e7c81ca748f289ac
Dmytro Farbun <dimofarb@gmail.com>
2025-08-15 21:06:19 +0300

    complete task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const { email, password } = form.elements;
+  const trimmedEmail = email.value.trim();
+  const trimmedPassword = password.value.trim();
+
+  if (!trimmedEmail || !trimmedPassword) {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  console.log({
+    userEmail: trimmedEmail,
+    userPassword: trimmedPassword
+  });
+
+  form.reset();
+}
