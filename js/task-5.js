de79a1b59719827ea7a1a0e86d9e69ee7679e6db
Dmytro Farbun <dimofarb@gmail.com>
2025-08-15 21:32:47 +0300

    complete task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,20 @@
+const MAX_COLOR_VALUE = 16777215;
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+  return `#${Math.floor(Math.random() * MAX_COLOR_VALUE)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function changeBackgroundColor() {
+  const newColor = getRandomHexColor();
+
+  body.style.backgroundColor = newColor;
+  spanColor.textContent = newColor;
+}
+
+const body = document.querySelector('body');
+const colorBtn = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+colorBtn.addEventListener('click', changeBackgroundColor);
